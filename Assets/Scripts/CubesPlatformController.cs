41060423a9613c93ef8f9a265f0b7b5f380c428f
Victor Farfan <vfarfan@ufm.edu>
2018-11-07 09:09:03 -0600

    Kaboom

---
+++ Assets/Scripts/CubesPlatformController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubesPlatformController : MonoBehaviour {
+
+	private float defaultRotation = 0f;
+	public float horizontalSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		defaultRotation = gameObject.transform.rotation.y;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		defaultRotation -= Input.GetAxis("Mouse X") * horizontalSpeed;
+		transform.rotation = Quaternion.Euler(0, defaultRotation, 0);
+		
+	}
+}
